aff033d52b58a0a57f15bf675dea5da94c59c02e
martinzutelman <zutelmanmartin@gmail.com>
2024-01-22 03:46:40 -0300

    Cool NavBar
    Added a fixed navbar on top with links/hashes to navigate vertically through the page, the navbar is not complete and its functioning is yet to be developed.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,44 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Header from "@/components/header";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Martin Zutel | Personal Portfolio',
-  description: 'Martin  Zutel is a full-stack developer',
-}
+  title: "Martin Zutel | Personal Portfolio",
+  description: "Martin  Zutel is a full-stack developer",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}  bg-slate-900 text-gray-200 relative flex items-center justify-center h-screen`}>
-        
-        <div className=' bg-purple-400 absolute top-[-4rem] right-[-60rem] h-[31.25rem] w-[60.25rem] rounded-full blur-[30rem]
-         sm:w-[68.75rem] transition-all duration-300 -z-10'></div>
+      <body
+        className={`${inter.className}  bg-slate-900 text-gray-200 relative`}
+      >
+        <div
+          className=" bg-purple-400 absolute top-[-4rem] right-[-60rem] h-[31.25rem] w-[60.25rem] rounded-full blur-[30rem]
+         sm:w-[68.75rem] transition-all duration-300 -z-10"
+        ></div>
 
-        <div className=' bg-blue-500 absolute top-[25rem]  left-[-60rem] h-[31.25rem] w-[60.25rem] rounded-full blur-[30rem]
-         sm:w-[68.75rem] md:left[-28rem] lg:left[-26rem] xl:left[-14rem] 2xl:lef[-4]  transition-all duration-300 -z-10'></div>
-
-        <div className='flex absolute font-extrabold text-5xl sm:text-9xl'>martin zutel</div>
+        <div
+          className=" bg-blue-500 absolute top-[25rem]  left-[-60rem] h-[31.25rem] w-[60.25rem] rounded-full blur-[30rem]
+         sm:w-[68.75rem] md:left[-28rem] lg:left[-26rem] xl:left[-14rem] 2xl:lef[-4]  transition-all duration-300 -z-10"
+        ></div>
         
+        <Header />
+
         {children}
       </body>
     </html>
-  )
+  );
 }
+
+
+
+  {/*<div className="flex absolute font-extrabold text-5xl sm:text-9xl">
+  martin zutel
+  </div>*/}
---
+++ components/header.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { links } from "@/lib/data";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="z-[999] relative">
+      <motion.div
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-10 shadow-lg shadow-black/[0.3]  backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+      ></motion.div>
+
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.15rem] sm:h-[initial] sm:py-0]">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text[0.9rem] font-medium text-white sm:w-[initial] sm:flex-nowrap sm:gap-4 ">
+          {links.map((link) => (
+            <li 
+            className="h-3/4 flex items-center justify-center"
+            key={link.hash}>
+              <Link className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-800 transition" href={link.hash}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header> 
+  );
+}
