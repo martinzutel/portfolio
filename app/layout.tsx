d336dbc377c90791d37f42093b8ff9b501fe76c3
martinzutelman <zutelmanmartin@gmail.com>
2024-01-21 23:32:16 -0300

    Type and cleaning
    Added some type to test and deleted some unnecessary files.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,13 +15,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}  bg-slate-900 text-gray-200 relative`}>
+      <body className={`${inter.className}  bg-slate-900 text-gray-200 relative flex items-center justify-center h-screen`}>
         
-        <div className=' bg-purple-400 absolute top-[-4rem] right-[-20rem] h-[31.25rem] w-[38.25rem] rounded-full blur-[16rem]
+        <div className=' bg-purple-400 absolute top-[-4rem] right-[-60rem] h-[31.25rem] w-[60.25rem] rounded-full blur-[30rem]
          sm:w-[68.75rem] transition-all duration-300 -z-10'></div>
 
-        <div className=' bg-blue-500 absolute top-[25rem]  left-[-30rem] h-[31.25rem] w-[38.25rem] rounded-full blur-[16rem]
+        <div className=' bg-blue-500 absolute top-[25rem]  left-[-60rem] h-[31.25rem] w-[60.25rem] rounded-full blur-[30rem]
          sm:w-[68.75rem] md:left[-28rem] lg:left[-26rem] xl:left[-14rem] 2xl:lef[-4]  transition-all duration-300 -z-10'></div>
+
+        <div className='flex absolute font-extrabold text-5xl sm:text-9xl'>martin zutel</div>
+        
         {children}
       </body>
     </html>
