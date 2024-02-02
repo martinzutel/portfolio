5967bac61195a84cf740226b210504a55b62c2a6
martinzutelman <zutelmanmartin@gmail.com>
2024-02-02 01:49:38 -0300

    slight design modifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,20 +17,20 @@ export default function RootLayout({
   return (
     <html lang="en" className=" overflow-x-hidden">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       </head>
       <body
         className={`${inter.className}  bg-black text-gray-200  relative h-[5000px] pt-28 sm:pt-36 overflow-x-hidden`}
         style={{ overflowX: "hidden" }}
       >
         <div
-          className=" bg-custom-red absolute top-[-4rem] right-[-60rem] h-[31.25rem] w-[60.25rem] rounded-full blur-[30rem]
-         sm:w-[68.75rem] transition-all duration-300 -z-10"
+          className=" bg-custom-red absolute top-[-4rem] right-[-60rem] h-[31.25rem] w-[60.25rem] rounded-full blur-[30rem] 
+         sm:w-[68.75rem] transition-all duration-300 "
         ></div>
 
         <div
           className="  bg-custom-red  absolute top-[25rem]  left-[-60rem] h-[31.25rem] w-[60.25rem] rounded-full blur-[30rem]
-         sm:w-[68.75rem] md:left[-28rem] lg:left[-26rem] xl:left[-14rem] 2xl:lef[-4rem]  transition-all duration-300 -z-10"
+         sm:w-[68.75rem] md:left[-28rem] lg:left[-26rem] xl:left[-14rem] 2xl:lef[-4rem]  transition-all duration-300"
         ></div>
         
         <Header />
