679e87d2ec8c83f1352d13594099baa75b57f255
martinzutelman <zutelmanmartin@gmail.com>
2024-02-01 21:31:37 -0300

    Relocated navbar on mobile to be prettier.

--- components/header.tsx
+++ components/header.tsx
@@ -8,12 +8,12 @@ export default function Header() {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className=" fixed top-0 left-1/2 h-[4.5rem] w-[22rem] rounded-full border-none border-white border-opacity-40 bg-white bg-opacity-10 shadow-lg shadow-black/[0.3]  backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+        className=" fixed top-3 left-1/2 h-[4.5rem] w-[22rem] rounded-full border-none border-white border-opacity-40 bg-white bg-opacity-10 shadow-lg shadow-black/[0.3]  backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
 
-      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.15rem] sm:h-[initial] sm:py-0]">
+      <nav className="flex fixed top-[0.85rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.15rem] sm:h-[initial] sm:py-0]">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text[0.9rem] font-medium text-white sm:w-[initial] sm:flex-nowrap sm:gap-4 ">
           {links.map((link) => (
             <motion.li 
